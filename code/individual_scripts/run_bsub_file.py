e2a85504c0776786c256bf6b54185b90e7965de7
ElishaGo <elishagoldstein0308@gmail.com>
2021-06-23 11:49:32 +0300

    fixed bugs

--- code/individual_scripts/run_bsub_file.py
+++ code/individual_scripts/run_bsub_file.py
@@ -157,7 +157,7 @@ def create_job_file(args):
     # run scRNAvar program
     f.write("# Run scrarevar program\n")
     f.write("python /home/labs/bioservices/shared/rarevar/code/scrarevar/code/scRNAvariants/scripts/scrnavariants.py"
-              " filtered_bam_files/{fname}_htseq_gene_header.bam {genome_ref} scRarevar_output/ --log-file log_files/log_{fname}.txt "
+              " filtered_bam_files/{fname}_htseq_gene_header.bam {genome_ref} scRarevar_output/ --log-file log_files/log_scRNA_rarvar_{fname}.txt "
               "--threads {n}\n\n".format(fname=args.fname, genome_ref=args.genome_ref, n=args.n))
 
     f.write('# Run statistics analysis program\n')
