42cc697d65ce09bc18fa9ebbc6559d4128fae25f
ElishaGo <elishagoldstein0308@gmail.com>
2022-05-18 16:37:09 +0300

    added intersection with gtf. added snp intersection with editing and gtf

--- scripts/step0_process_editing_DB_file.py
+++ scripts/step0_process_editing_DB_file.py
@@ -9,6 +9,7 @@ for more information: https://bedtools.readthedocs.io/en/latest/content/overview
 """
 import argparse
 import os
+import subprocess
 from datetime import datetime
 import pandas as pd
 import numpy as np
@@ -20,13 +21,6 @@ class BadFileHeaderError(Exception):
     pass
 
 
-def load_file(edit_orig_DB_path):
-    df = sc_rna_variants.analysis_utils.load_df(edit_orig_DB_path)
-    df.rename(columns={df.columns[0]: "#chrom", df.columns[1]: 'chromEnd', 'Strand': 'strand'}, inplace=True)
-    df = df.fillna('.')
-    return df
-
-
 def check_editing_DB_header(editing_file_path):
     with open(editing_file_path) as f:
         first_line = f.readline()
@@ -34,15 +28,15 @@ def check_editing_DB_header(editing_file_path):
     return good_header
 
 
-def load_and_process_fasta_coordinations(editing_DB_bed, fasta_path, editing_orig_DB_path):
+def load_and_process_fasta_coordinations(editing_DB_bed, fasta_path, editing_DB_path):
     """
     find referenes from fasta and return as dataframe.
     We get the references from the fasta file by using bedtools getfasta which outputs file in the format:
     '>chr1:87157-87158(+)\n',
     't\n', ...
     """
-    temp_ref_path = editing_orig_DB_path[:editing_orig_DB_path.rfind(os.sep)] + '/temp_ref_from_fasta_by_bed_file.txt'
-    temp_editing_bed_path = editing_orig_DB_path[:editing_orig_DB_path.rfind(os.sep)] + '/editing_DB.bed'
+    temp_ref_path = editing_DB_path[:editing_DB_path.rfind(os.sep)] + '/temp_ref_from_fasta_by_bed_file.txt'
+    temp_editing_bed_path = editing_DB_path[:editing_DB_path.rfind(os.sep)] + '/editing_DB.bed'
     if not os.path.isfile(temp_ref_path):
         print("running bedtools getfasta")
         editing_DB_bed.to_csv(temp_editing_bed_path, index=False, sep='\t')
@@ -72,7 +66,7 @@ def sort_and_reorder(df):
     df = df.sort_values(by=['#chrom', 'chromStart'])
 
     # reorder columns
-    columns_reorder = ['#chrom', 'chromStart', 'chromEnd', 'Ref', 'empty_numeric_column', 'strand', 'Ed', 'db', 'type',
+    columns_reorder = ['#chrom', 'chromStart', 'chromEnd', 'Ref', 'score', 'strand', 'Ed', 'db', 'type',
                        'dbsnp', 'repeat',
                        'Func.wgEncodeGencodeBasicV34', 'Gene.wgEncodeGencodeBasicV34',
                        'GeneDetail.wgEncodeGencodeBasicV34', 'ExonicFunc.wgEncodeGencodeBasicV34',
@@ -83,12 +77,12 @@ def sort_and_reorder(df):
 
 
 def merge_DB_and_fasta_by_coor(editing_DB, fasta_coor):
-    print("start merge")
-    return pd.merge(editing_DB.drop_duplicates().reset_index(), fasta_coor.drop_duplicates(), how='inner').set_index('index')
+    return pd.merge(editing_DB.drop_duplicates().reset_index(), fasta_coor.drop_duplicates(), how='inner').set_index(
+        'index')
 
 
-def split_editing_by_A_reference(editing_DB_bed, fasta_path, editing_orig_DB_path):
-    fasta_coordinates_df = load_and_process_fasta_coordinations(editing_DB_bed, fasta_path, editing_orig_DB_path)
+def split_editing_by_A_reference(editing_DB_bed, fasta_path, editing_DB_path):
+    fasta_coordinates_df = load_and_process_fasta_coordinations(editing_DB_bed, fasta_path, editing_DB_path)
 
     # TODO: ask Dena about filter by strand. currently we ignore here strandness
     fasta_A_I = fasta_coordinates_df[fasta_coordinates_df['Ref'] == 'a']
@@ -120,38 +114,75 @@ def transform_to_bed(editing_DB_df):
                        'GeneDetail.knownGene', 'ExonicFunc.knownGene', 'AAChange.knownGene', 'phastConsElements100way']
     editing_DB_bed_df = editing_DB_df[columns_reorder]
 
-    editing_DB_bed_df.insert(4, 'empty_numeric_column', np.int8(0))  # add numeric column to keep bed format
+    editing_DB_bed_df.insert(4, 'score', np.int8(0))  # add numeric column to keep bed format
 
     return editing_DB_bed_df
 
 
-def run(args):
+def intersect_with_gtf(a_path, gtf_path, output_path):
+    subprocess.run(['bedtools', ' intersect', '-a', a_path, '-b', gtf_path, '-o', output_path])
+
+
+def process_editing_DB(editing_DB_path, fasta_path, gtf_path):
     # TODO: check how to change the header on the fly
-    good_header = check_editing_DB_header(args.editing_orig_DB_path)
+    good_header = check_editing_DB_header(editing_DB_path)
     if not good_header:
         raise BadFileHeaderError("Need to add '#' to the beginning of the header")
 
-    editing_DB_df = load_file(args.editing_orig_DB_path)
+    editing_DB_df = sc_rna_variants.analysis_utils.load_df(editing_DB_path)
 
     editing_DB_bed = transform_to_bed(editing_DB_df)
 
-    editing_A_I, editing_other = split_editing_by_A_reference(editing_DB_bed, args.fasta_path,
-                                                              args.editing_orig_DB_path)
+    editing_A_I, editing_other = split_editing_by_A_reference(editing_DB_bed, fasta_path,
+                                                              editing_DB_path)
 
     editing_A_I = sort_and_reorder(editing_A_I)
     editing_other = sort_and_reorder(editing_other)
 
     # save the editing DB file as bed file
-    output_dir = args.editing_orig_DB_path[:args.editing_orig_DB_path.rfind(os.sep)]
-    editing_A_I.to_csv(os.path.join(output_dir, "0_editing_A_I.bed6"), sep='\t', index=False)
-    editing_other.to_csv(os.path.join(output_dir, "0_editing_other.bed6"), sep='\t', index=False)
+    output_dir = editing_DB_path[:editing_DB_path.rfind(os.sep)]
+    editing_A_I_path = os.path.join(output_dir, "0_editing_A_I.bed6")
+
+    sc_rna_variants.analysis_utils.save_df(editing_A_I, output_dir, "0_editing_A_I.bed6")
+    sc_rna_variants.analysis_utils.save_df(editing_other, output_dir, "0_editing_other.bed6")
+
+    # intersect with gencode.gtf file
+    intersect_with_gtf(a_path=editing_A_I_path, gtf_path=gtf_path,
+                       output_path=os.path.join(output_dir, '0_editing_A_I.genecode_intersect.bed6'))
+
+
+def snp_DB_intersections(snp_DB_path, editing_DB_path, gtf_path):
+    """
+    make intersection of the SNP DB with the entries in the editing files with reference base 'A', and make another
+    intersection with the gencode (transcriptome) file
+    :param snp_DB_path:
+    :param editing_DB_path:
+    :param gtf_path:
+    :return:
+    """
+    # intersect with editing_A_I file
+    snp_A_output = os.path.join(snp_DB_path[:snp_DB_path.rfind(os.sep)], '0_snp_A.vcf')
+    subprocess.run(['bedtools', 'intersect', '-u', '-header', '-a', snp_DB_path, '-b',
+                    os.path.join(editing_DB_path, '0_editing_A_I.bed6'), '>', snp_A_output])
+
+    # instersect with gtf file
+    snp_A_gtf_output = os.path.join(snp_DB_path[:snp_DB_path.rfind(os.sep)], '0_snp_A.gencode_intersect.vcf')
+    subprocess.run(
+        ['bedtools', 'intersect', '-u', '-header', '-a', snp_A_output, '-b', gtf_path, '>', snp_A_gtf_output])
+
+
+def run_step0(args):
+    process_editing_DB(args.editing_DB_path, args.fasta_path, args.gtf_path)
+    snp_DB_intersections(args.snp_DB_path, args.editing_DB_path, args.gtf_path)
 
 
 def parse_arguments(arguments=None):
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('editing_orig_DB_path')
+    parser.add_argument('editing_DB_path')
     parser.add_argument('fasta_path')
+    parser.add_argument('gtf_path')
+    parser.add_argument('snp_DB_path')
 
     return parser.parse_args(arguments)
 
@@ -160,6 +191,6 @@ if __name__ == '__main__':
     startTime = datetime.now()
 
     args = parse_arguments()
-    run(args)
+    run_step0(args)
     print(datetime.now() - startTime)
-    print("\nfinished to process editing DB file")
+    print("\nfinished to process DB files")
